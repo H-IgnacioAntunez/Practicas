ca8a3bb2b331f77ac87c0eb0acf3d58473c3320f
H-IgnacioAntunez <ignacioantunezrn@gmail.com>
2023-10-19 15:26:47 -0300

    Agregado form netlify

---
+++ PrepCourseHenry/Variables.js
@@ -0,0 +1,24 @@
+var cajon;
+
+//String
+var bootcamp= `henry`;
+
+//Numeros
+var numeroEntero = 17;
+var numeroNegativo = -17;
+var numeroConComa= 19.3;
+
+//Booleanos
+var verdadero= true;
+var falso = false;
+
+//Undefined
+//Cuando aun no se asigna valor a la variable.
+var datoVacio;
+
+//Null
+//Cuando se indica explicitamente que el valor este vacio
+var vacio= null;
+
+let texto = prompt("Ingrese un texto").length;
+document.write(`La longitud del texto ingresado es de ${texto}`);
---
+++ PrepCourseHenry/operadores.js
@@ -0,0 +1,4 @@
+let suma = 3 + 5;
+console.log(`El resultado de la suma es ${suma}`);
+let resta = 14-7;
+console.log(`El resultado de la suma es ${suma}`);
